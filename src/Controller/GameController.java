8b8ffd68266960388c4ad6f3bd8fde4ffec9da38
Dobri <dobri@Dobris-MacBook-Pro.local>
2021-04-28 02:18:29 +0300

    Objects falling and stacking working. Game is partly playable.

--- src/Controller/GameController.java
+++ src/Controller/GameController.java
@@ -1,8 +1,11 @@
 package controller;
 
+import java.awt.Color;
 import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import java.util.Date;
 import java.util.LinkedList;
 
@@ -16,8 +19,12 @@ public class GameController
 	private static JLabel currentObject = new JLabel();
 	private static LinkedList<JLabel> objects = new LinkedList<>();
 	private static int currentObjectRow = 0;
-	private static int currentObjectColumn;
+	private static int currentObjectColumn = 0;
+	private static int currentObjectX = 0;
+	private static int currentObjectY = 0;
 	static boolean closeT = false;
+	static boolean hasMovedLeft = false;
+	static boolean hasMovedRight = false;
 	public static void gameStart()
 	{
 		startDropping();
@@ -30,7 +37,6 @@ public class GameController
 	}
 	private static void createRandomObject()
 	{
-		//Creates only one type for now
 		currentObject = new JLabel("█");
 		currentObject.setFont(new Font("Serif", Font.PLAIN, 40));
 		currentObject.setBounds(20, -200, 40, 40);
@@ -40,20 +46,23 @@ public class GameController
 	
 	private static void objectStartFalling()
 	{
-		
 		ActionListener objectsDropper = new ActionListener() {
+			
 	        public void actionPerformed(ActionEvent evt) {
 	        	
 	        	
 	        	dropObjectOneRowDown();
-	        	printFieldOnConsole();
+	        	keyPressEvent();
 	        	fieldUpdate();
 	        	if(currentObjectRow >= 19 || currentObjectHasCollided())
 	        	{
 	        		createRandomObject();
 	        		currentObjectRow = 0;
+	        		currentObjectColumn = 8;
+	        		currentObjectX = 240;
+	        		currentObjectY = 0;
+	        		printFieldOnConsole();
 	        	}
-	                
 	        }
 	    };
 	    javax.swing.Timer t = new javax.swing.Timer(1* 200, objectsDropper);
@@ -62,11 +71,10 @@ public class GameController
 	}
 	private static void dropObjectOneRowDown()
 	{
-		currentObjectColumn = getCurrentObjectColumn();
 		
 		
-		currentObject.setBounds(20, currentObjectRow*39, 100, 100);
-		
+		currentObjectY += 39;
+		currentObject.setBounds(currentObjectX, currentObjectY, 100, 100);
 		currentObject.repaint();
         FrameBuilder.mainFrame.repaint();
         FrameBuilder.mainFrame.getContentPane().repaint();
@@ -98,50 +106,65 @@ public class GameController
 	}
 	private static void fieldUpdate()
 	{
+		
 		if(currentObjectRow - 1 >=0)
 			GameStatus.field[currentObjectRow-1][currentObjectColumn] = 0;
 		
-		GameStatus.field[currentObjectRow][currentObjectColumn] = 1;
+		//GameStatus.field[currentObjectRow][currentObjectColumn] = 1;
+		
+		if(hasMovedLeft && currentObjectColumn-1 >=0)
+		{
+			if(currentObjectColumn+1 <= 9)
+				GameStatus.field[currentObjectRow][currentObjectColumn+1] = 0;
+			
+		//	GameStatus.field[currentObjectRow][currentObjectColumn-1] = 1;
+			hasMovedLeft = false;
+			currentObjectX -= 30;
+			currentObjectColumn --;
+			System.out.println("left");
+		}else
+		if(hasMovedRight&& currentObjectColumn+1 <= 9)
+		{
+			if(currentObjectColumn-1 >= 0)
+				GameStatus.field[currentObjectRow][currentObjectColumn-1] = 0;
+			
+			//GameStatus.field[currentObjectRow][currentObjectColumn+1] = 1;
+			hasMovedRight = false;
+			currentObjectX += 30;
+			currentObjectColumn ++;
+			System.out.println("right");
+		}else 
+		{
+			GameStatus.field[currentObjectRow][currentObjectColumn] = 1;
+			
+		}
+		
 		
 	}
 	
-	private static int getCurrentObjectColumn()
-	{
-		int x = currentObject.getX();
-		if(x >=0 && x <30)
-		{
-			return 0;
-		}else if(x >=30&& x <60)
-		{
-			return 1;
-		}else if(x >=30&& x <60)
-		{
-			return 2;
-		}else if(x >=60&& x <90)
-		{
-			return 3;
-		}else if(x >=90&& x <120)
-		{
-			return 4;
-		}else if(x >=120&& x <150)
-		{
-			return 5;
-		}else if(x >=150&& x <180)
-		{
-			return 6;
-		}else if(x >=180&& x <210)
-		{
-			return 7;
-		}else if(x >=210&& x <240)
-		{
-			return 8;
-		}else if(x >=240&& x <270)
-		{
-			return 9;
-		}else if(x >=270)
-		{
-			return 10;
-		}
-		return -1;
+	 private static void keyPressEvent() 
+	 {
+		 FrameBuilder.mainFrame.addKeyListener(new KeyListener() {
+             @Override
+             public void keyTyped(KeyEvent e) {
+             }
+
+             @Override
+             public void keyPressed(KeyEvent e)
+             {
+        		 	if(e.getKeyCode() == 37)
+        		 		hasMovedLeft = true;
+        		 	
+        		 	if(e.getKeyCode() == 39)
+        		 		hasMovedRight = true;
+             }
+
+             @Override
+             public void keyReleased(KeyEvent e) {
+             }
+         });
+
+		
+	 }
+
 	}
-}
